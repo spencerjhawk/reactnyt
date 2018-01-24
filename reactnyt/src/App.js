29e28cce7c65bee54416dc12d8c41d0886df047e
Spencer Hawk <spencerjhawk@gmail.com>
2018-01-24 15:27:32 -0600

    necessary app mods

--- reactnyt/src/App.js
+++ reactnyt/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Search from './components/Search';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      savedArticles: []
+    };
+  }
+
+  componentDidMount() {
+    
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <div className="App-header row center-align">
+          <h2>Article Scrubber</h2>
+          <p className="App-intro">
+            Search for and annotate articles from the internet
+          </p>
+        </div>
+        <Search />
+        
       </div>
     );
   }
